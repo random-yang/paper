9c03858715127044ba97d4e66534fb3e355cff8a
OfficialYoungX <1070381686@qq.com>
2019-11-22 23:02:35 +0800

    [+]: add switch transition

--- source/js/paper.js
+++ source/js/paper.js
@@ -4,60 +4,57 @@ const invertImgs = (function() {
   return invertImgs
 })()
 
-const addMixBlendModeMask = (color = '#000000', mode = 'screen') => {
-  let body = document.body
-  // create mask
-  let mask = document.createElement('div')
-  // set the mask css property
-  let maskCss = {
-      width: '100%',
-      height: '100%',
-      backgroundColor: color,
-      position: 'fixed',
-      top: '0',
-      left: '0',
-      mixBlendMode: mode,
-      zIndex: 10,
-      pointerEvents: 'none',
-      transition: 'background-color 0.3s ease-out'
-  }
-  for (property in maskCss) {
-    mask.style[property] = maskCss[property]
-  }
-  mask.id = 'darkmode-mask'
-  // insert the musk into the page body
-  body.append(mask);
+// const addMixBlendModeMask = (color = '#000000', mode = 'screen') => {
+//   let body = document.body
+//   // create mask
+//   let mask = document.createElement('div')
+//   // set the mask css property
+//   let maskCss = {
+//       width: '100%',
+//       height: '100%',
+//       backgroundColor: color,
+//       position: 'fixed',
+//       top: '0',
+//       left: '0',
+//       mixBlendMode: mode,
+//       zIndex: 10,
+//       pointerEvents: 'none',
+//       transition: 'background-color 0.3s ease-out'
+//   }
+//   for (property in maskCss) {
+//     mask.style[property] = maskCss[property]
+//   }
+//   mask.id = 'darkmode-mask'
+//   // insert the musk into the page body
+//   body.append(mask);
+// }
+
+function Darkmode() {
+  this.mask = document.getElementById('darkmode-mask')
+  this.imgs = document.getElementsByTagName('img')
 }
-
-const setDarkmode = (() => {
-  const turnOnDarkmode = () => {
-    // fix chrome bug
-    document.querySelector('html').style.background = '#ffffff'
-    addMixBlendModeMask('rgba(255,255,255,1)','difference')
-    // invert the images color
-    // so that they can render with the right color
-    invertImgs(document.getElementsByTagName('img'))
-  }
-  const turnOffDarkmode = () => {
-    const mask = document.getElementById('darkmode-mask')
-    invertImgs(document.getElementsByTagName('img'))
-    document.body.removeChild(mask)
-  }
-
-  return {
-    turnOnDarkmode,
-    turnOffDarkmode
-  }
-})()
+Darkmode.prototype.turnOnDarkmode = function() {
+  // fix chrome bug
+  document.querySelector('html').style.background = '#ffffff'
+  this.mask.classList.add('darkmode-mask--dark');
+  // invert the images color
+  // so that they can render with the right color
+  invertImgs(this.imgs)
+}
+Darkmode.prototype.turnOffDarkmode = function() {
+  this.mask.classList.remove('darkmode-mask--dark');
+  invertImgs(this.imgs)
+}
 
 
 window.addEventListener('DOMContentLoaded', () => {
   const checkbox = document.querySelector('input[name=mode]');
+  const darkmode = new Darkmode()
   checkbox.addEventListener('change', function() {
     if(this.checked) {
-      setDarkmode.turnOnDarkmode()
+      darkmode.turnOnDarkmode()
     } else {
-      setDarkmode.turnOffDarkmode()
+      darkmode.turnOffDarkmode()
     }
   })
 })
