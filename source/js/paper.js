bc2b301efeea9c7770d5d40ccbb2fa0fe69d83eb
OfficialYoungX <1070381686@qq.com>
2019-11-23 11:01:28 +0800

    [*]: update the ico icon

--- source/js/paper.js
+++ source/js/paper.js
@@ -4,31 +4,6 @@ const invertImgs = (function() {
   return invertImgs
 })()
 
-// const addMixBlendModeMask = (color = '#000000', mode = 'screen') => {
-//   let body = document.body
-//   // create mask
-//   let mask = document.createElement('div')
-//   // set the mask css property
-//   let maskCss = {
-//       width: '100%',
-//       height: '100%',
-//       backgroundColor: color,
-//       position: 'fixed',
-//       top: '0',
-//       left: '0',
-//       mixBlendMode: mode,
-//       zIndex: 10,
-//       pointerEvents: 'none',
-//       transition: 'background-color 0.3s ease-out'
-//   }
-//   for (property in maskCss) {
-//     mask.style[property] = maskCss[property]
-//   }
-//   mask.id = 'darkmode-mask'
-//   // insert the musk into the page body
-//   body.append(mask);
-// }
-
 function Darkmode() {
   this.mask = document.getElementById('darkmode-mask')
   this.imgs = document.getElementsByTagName('img')
@@ -48,7 +23,7 @@ Darkmode.prototype.turnOffDarkmode = function() {
 
 
 window.addEventListener('DOMContentLoaded', () => {
-  const checkbox = document.querySelector('input[name=mode]');
+  const checkbox = document.querySelector('input[name=mode]')
   const darkmode = new Darkmode()
   checkbox.addEventListener('change', function() {
     if(this.checked) {
